6952c0e5b3a4b42628084021ada268efaee6ccad
Vítor Mangaravite <vitordouzi@gmail.com>
2022-11-18 17:49:06 -0300

    Version with MultiHeaded-Distance

--- netc/methods/tfidfatt/AttentionTFIDF.py
+++ netc/methods/tfidfatt/AttentionTFIDF.py
@@ -18,53 +18,89 @@ from .Similarities import SimMatrix, DistMatrix
 from ...metrics.tick import Tick
 
 class AttentionTFIDF(nn.Module):
-    def __init__(self, vocab_size: int, nclass: int, hiddens: int=300, maxF: int=20, drop: float = .5,
-                 alpha: float = 0.25, gamma: float = 3., reduction: str = 'sum', 
+    def __init__(self, vocab_size: int, hiddens: int, nclass: int, maxF: int=20, nheads: int=6,
+                 alpha: float = 0.25, gamma: float = 3., reduction: str = 'sum', drop: float = .5,
                 att_model: str ='AA', sim_func='dist', norep=2):
         super(AttentionTFIDF, self).__init__()
-        #self.fc        = nn.Linear(hiddens, nclass+1)
-        self.fc       = nn.Sequential( nn.Linear(hiddens, nclass+norep), nn.Softmax(dim=-1))
+        self.D   = hiddens    # number of   (D)imensions
+        self.C   = nclass     # number of   (C)lass
+        self.H   = nheads     # number of   (H)eads on multhead
+        self.V   = vocab_size # size of the (V)ocabulary
+        self.P   = norep
+        #self.norm     = nn.LayerNorm(self.H)
+        self.norm     = nn.BatchNorm1d(self.H)
+        self.fc       = nn.Sequential( nn.Linear(self.D, self.C+self.P), nn.Softmax(dim=-1))
         self.drop_    = nn.Dropout(drop)
-        self.emb_     = EmbbedingTFIDF(vocab_size, hiddens, maxF=20, drop=self.drop_,  att_model=att_model)
+        self.emb_     = EmbbedingTFIDF(vocab_size, self.D, maxF=maxF, drop=self.drop_,  att_model=att_model)
         self.loss_f   = FocalLoss(gamma=gamma, alpha=alpha, reduction=reduction)
-        self.nclass   = nclass
-        self.bias     = nn.parameter.Parameter(torch.zeros(nclass+norep))
-        if sim_func == 'dist':
-            self.sim_func = DistMatrix()
-        elif sim_func == 'sim':
-            self.sim_func = SimMatrix()
+        self.dist_func = DistMatrix()
         self.init_weights()
     
     def init_weights(self):
         nn.init.xavier_normal_(self.fc[0].weight.data)
         
+    def getHidden(self, hidd):
+        B, L, D = hidd.shape
+        assert D == self.D
+        hidd = hidd.view( B, L, self.H, self.D//self.H )
+        hidd = hidd.transpose(1,2)
+        return hidd
+    
+    def catHiddens(self, hidd):
+        B, H, L, D = hidd.shape
+        assert H*D == self.D
+        assert H == self.H
+        hidd = hidd.transpose(1,2)
+        hidd = hidd.reshape(B, L, H*D)
+        return hidd
+    def removeNaN(self, data):
+        zeros   = torch.zeros_like(data)
+        isnan   = torch.isnan(data)
+        return torch.where(isnan, zeros, data)
+    def lnormalize(self, co_weights):
+        B,H,L,_ = co_weights.shape
+        nco_weights = co_weights.view(B,H,L*L).transpose(-1,-2)
+        nco_weights = self.norm(nco_weights)
+        return nco_weights.transpose(-1,-2).view(B,H,L,L)
+    def bnormalize(self, co_weights):
+        B,H,L,_ = co_weights.shape
+        nco_weights = co_weights.reshape(B,H,L*L)
+        nco_weights = self.norm(nco_weights)
+        return nco_weights.reshape(B,H,L,L)
+    
     def forward(self, doc_tids, TFs, DFs, labels=None):
         
-        result = self.emb_(doc_tids, TFs, DFs)
+        result = self.emb_(doc_tids, TFs, DFs) 
+        K = self.getHidden(result['K']) # K:[B,L,D] -> L:[B,H,L,D//H]
+        Q = self.getHidden(result['Q']) # Q:[B,L,D] -> Q:[B,H,L,D//H]
+        B,H,L,_ = Q.shape
         
-        co_weights  = self.sim_func( result['K'], result['Q'] ) # SIMILARITY(Q:[B,L,D], Q:[B,L,D]) -> W:[B,L,L]
-        co_weights[result['pad_mask'].logical_not()] = 0.
+        co_weights  = self.dist_func( K, Q ) # SIMILARITY(Q:[B,H,L,D//H], Q:[B,H,L,D//H]) -> W:[B,H,L,L]
+        co_weights  = self.bnormalize(co_weights)
         
-        V = torch.softmax(co_weights, dim=-1) @ result['V']    # W:[B,L,L] @ V:[B,L,D] -> V':[B,L,D]
+        pad_mask = result['pad_mask'].unsqueeze(1).repeat([1, H, 1, 1]) # pm:[B, L, L] -> pm:[B, H, L, L]
+        co_weights[pad_mask.logical_not()] = 0.
+        co_weights  = torch.softmax(co_weights, dim=-1)                     # co:[B, H, L, L]
+        co_weights = self.removeNaN(co_weights)
+        
+        V = self.getHidden(result['V'])
+        V = co_weights @ V    # W:[B,H,L,L] @ V:[B,H,L,D//H] -> V':[B,H,L,D//H]
         V = self.drop_(V)
-        V_lgts = self.fc(V)                                    # FC(V':[B,L,D])-> V_lgs:[B,L,C+b]
+        V_lgts = self.fc(self.catHiddens(V))         # FC(V':[B,L,D])-> V_lgs:[B,L,C+b]
         
-        weights = co_weights.sum(axis=-2)                       # sum(co_weights:[B,L,L], 1) -> weights:[B,L]
-        weights = weights / result['doc_sizes']                 # weights:[B,L] / d_sizes:[B,1]
+        weights = co_weights.sum(axis=-2).mean(axis=-2)    # sum(co_weights:[B,H,L,L], -2) -> weights:[B,L]
+
+        weights = weights / result['doc_sizes']                   # weights:[B,L] / d_sizes:[B,1]
+        #bx_pack = result['bx_packed'].unsqueeze(1).repeat([1,self.H,1])
         weights[result['bx_packed']] = float('-inf')
-        weights = torch.softmax(weights, dim=-1)                 # softmax(weights:[B,L]) -> weights:[B,L]
+        weights = torch.softmax(weights, dim=-1)     # softmax(weights:[B,L]) -> weights:[B,L]
+        weights = self.removeNaN(weights).unsqueeze(-1)  # weights:[B,L] -> weights:[B,L,1]
         
-        zeros   = torch.zeros_like(weights)
-        isnan   = torch.isnan(weights)
-        weights = torch.where(isnan, zeros, weights)
-        
-        weights = weights.view( *weights.shape, 1 )            # weights:[B,L] -> weights:[B,L,1]
-        
-        V_lgts = V_lgts * weights                              # V_lgs:[B,L,C+1] * weights:[B,L,1] -> logits:[B,L,C+1]
-        
-        logits = V_lgts.sum(dim=1)[:,:self.nclass]             # V_lgts:logits:[B,L,C+b] -> logits:[B,C]
-        logits = torch.softmax(logits, dim=-1)                 # softmax(logits:[B,C]) -> logits:[B,C]
-        
+        V_lgts = V_lgts * weights                        # V_lgs:[B,L,C+1] * weights:[B,L,1] -> logits:[B,L,C+1]
+
+        logits = V_lgts.sum(dim=-2)[:,:self.C]         # V_lgts:logits:[B,L,C+b] -> logits:[B,C]
+        logits = torch.softmax(logits, dim=-1)           # softmax(logits:[B,C]) -> logits:[B,C]
+         
         result_ = { 't_probs': V_lgts, 'logits': logits}
         if labels is not None:
             result_['loss'] = self.loss_f(logits, labels)
