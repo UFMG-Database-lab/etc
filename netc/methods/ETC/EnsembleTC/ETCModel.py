2ec98ef0dbb26374d7420278dbc0d430c2bffc53
Vítor Mangaravite <vitordouzi@gmail.com>
2022-12-20 11:11:58 -0300

    New Emb+nAtt+ETC model

--- netc/methods/ETC/EnsembleTC/ETCModel.py
+++ netc/methods/ETC/EnsembleTC/ETCModel.py
@@ -5,26 +5,25 @@ from ..Similarities import DistMatrix
 from torch import nn
 import torch
 
+def removeNaN(data):
+    zeros   = torch.zeros_like(data)
+    isnan   = torch.isnan(data)
+    return torch.where(isnan, zeros, data)
+
 class nearAttention(nn.Module):
     def __init__(self, hiddens: int, nheads: int=6):
         super(nearAttention, self).__init__()
         self.H = nheads
         self.D = hiddens
+        self.d = hiddens // nheads
         self.dist_func = DistMatrix()
-        self.norm1     = nn.BatchNorm1d(self.H)
-        self.norm2     = nn.BatchNorm1d(self.H)
-        self.posterior = nn.Sequential( nn.Linear(self.H, 2), nn.Softmax(dim=-1) )
+        self.norm     = nn.BatchNorm1d(self.H)
         
     def getHidden(self, hidd):
-        B, L, D = hidd.shape
-        assert D == self.D
-        hidd = hidd.view( B, L, self.H, self.D//self.H )
-        hidd = hidd.transpose(1,2)
+        B,L,_ = hidd.shape
+        hidd  = hidd.view( B, L, self.H, self.d )
+        hidd  = hidd.transpose(1,2)
         return hidd
-    def removeNaN(self, data):
-        zeros   = torch.zeros_like(data)
-        isnan   = torch.isnan(data)
-        return torch.where(isnan, zeros, data)
     def forward(self, Q, K, V, pad_mask, bx_packed, **kargs):
         Q = self.getHidden(Q) # Q:[B,L,D] -> Q:[B,H,L,d]
         K = self.getHidden(K) # K:[B,L,D] -> K:[B,H,L,d]
@@ -36,23 +35,14 @@ class nearAttention(nn.Module):
         # co_weights (cW)
         co_weights = self.dist_func( K, Q )              # SIMILARITY(Q:[B,H,L,D//H], Q:[B,H,L,D//H]) -> cW:[B,H,L,L] 
         co_weights = co_weights.reshape(B,H,L*L)         # cW:[B,H,L,L] -> cW:[B,H,L*L]
-        co_weights = self.norm1(co_weights)              # batchNorm(cW:[B,H,L*L]) -> cW:[B,H,L*L]
+        co_weights = self.norm(co_weights)               # batchNorm(cW:[B,H,L*L]) -> cW:[B,H,L*L]
         co_weights = co_weights.reshape(B,H,L,L)         # cW:[B,H,L*L] -> cW:[B,H,L,L]
         co_weights[pad_mask] = 0.                        # fill(cW:[B,H,L,L], pad)
         co_weights = torch.softmax(co_weights, dim=-1)   # softmax(cW:[B, H, L, L]) -> cW:[B, H, L, L']
-        co_weights = self.removeNaN(co_weights)          # fill(cW:[B,H,L,L'], NaN)
+        co_weights = removeNaN(co_weights)               # fill(cW:[B,H,L,L'], NaN)
         
-        # weights (w)
-        weights = co_weights.sum(axis=-2)                # sum(cW:[B,H,(L),L], -2) -> w:[B,H,L]
-        weights = self.norm2(weights).transpose(1,2)     # batchNorm(w:[B,H,L]).T -> w:[B,L,H]
-        weights = self.posterior(weights)                # P(w:[B,L,H]|C) -> w:[B,L,2]
-        weights = weights[:,:,0].squeeze(-1)             # w:[B,L,2] -> w:[B,L,1] -> w:[B,L]
-        weights = self.removeNaN(weights)                # fill(w:[B,L,1], NaN)
-        weights[bx_packed] = float('-inf')               # fill(w:[B,L,1], packed)
-        weights = torch.softmax(weights, dim=-1)         # softmax(w:[B,L]) -> w:[B,L]
-        weights = self.removeNaN(weights).unsqueeze(-1)  # fill(w:[B,L], NaN) -> w:[B,L] -> w:[B,L,1]
-        
-        return { 'co_weights': co_weights, 'weights': weights, 'V': V } # 
+        return { 'co_weights': co_weights, 'bx_packed': bx_packed, 'V': V } # 
+
 class EnsembleTC(nn.Module):
     def __init__(self, drop, hiddens: int, nclass:int, norep:int = 2, nheads: int=6, dev: bool=False):
         super(EnsembleTC, self).__init__()
@@ -61,6 +51,8 @@ class EnsembleTC(nn.Module):
         self.C    = nclass
         self.P    = norep
         self._dev = dev
+        self.norm = nn.BatchNorm1d(self.H)
+        self.posterior = nn.Sequential( nn.Linear(self.H, 2), nn.Softmax(dim=-1) )
         self.fc   = nn.Sequential( nn.Linear(self.D, self.C+self.P), nn.Softmax(dim=-1))
         
     def catHiddens(self, hidd):
@@ -70,12 +62,23 @@ class EnsembleTC(nn.Module):
         hidd = hidd.transpose(1,2)
         hidd = hidd.reshape(B, L, H*d)
         return hidd
-    def forward(self, V, co_weights, weights):
+    def forward(self, V, co_weights, bx_packed):
+        
+        # weights (w)
+        weights = co_weights.sum(axis=-2)                # sum(cW:[B,H,(L),L], -2) -> w:[B,H,L]
+        weights = self.norm(weights).transpose(1,2)      # batchNorm(w:[B,H,L]).T -> w:[B,L,H]
+        weights = self.posterior(weights)                # P(w:[B,L,H]|C) -> w:[B,L,2]
+        weights = weights[:,:,0].squeeze(-1)             # w:[B,L,2] -> w:[B,L,1] -> w:[B,L]
+        weights = removeNaN(weights)                     # fill(w:[B,L,1], NaN)
+        weights[bx_packed] = float('-inf')               # fill(w:[B,L,1], packed)
+        weights = torch.softmax(weights, dim=-1)         # softmax(w:[B,L]) -> w:[B,L]
+        weights = removeNaN(weights).unsqueeze(-1)       # fill(w:[B,L], NaN) -> w:[B,L] -> w:[B,L,1]
+        
         if self._dev:
             old_V_lgts = self.fc(self.catHiddens(V)) * weights
         V = co_weights @ V                                      # W:[B,H,L,L] @ V:[B,H,L,d] -> V':[B,H,L,d]
         V = self.catHiddens(V)                                  # V':[B,H,L,d] -> V':[B,L,D]
-        V_lgts = self.fc(V)                                     # V':[B,H,L,d] -> P:[B,L,C+P]
+        V_lgts = self.fc(V)                                     # V':[B,H,L,d] -> P:[B,L,C+P]float('-inf')
         V_lgts = V_lgts * weights                               # P:[B,L,C+P] * w:[B,H,L] -> P:[B,L,C+P]
         logits = V_lgts.sum(dim=-2)[:,:self.C]                  # logits:[B,L,C+P] -> logits:[B,C+P] -> logits:[B,C]
         logits = torch.softmax(logits, dim=-1)                  # softmax(logits:[B,C]) -> logits:[B,C]
@@ -85,6 +88,7 @@ class EnsembleTC(nn.Module):
             returing['old_V_lgts'] = old_V_lgts
         
         return returing
+
 class ETCModel(nn.Module):
     def __init__(self, vocab_size: int, hiddens: int, nclass: int, maxF: int=20, nheads: int=6,
                  alpha: float = 0.25, gamma: float = 3., reduction: str = 'sum', drop: float = .5,
